e2f4ad7c12698666ae7043fa607b05d880b3cd3b
YossiSella <66948150+YossiSella@users.noreply.github.com>
2020-09-30 17:04:02 +0300

    Add files via upload
    base project to build on

---
+++ semantic-segmentation-pytorch-master/scratch.py
@@ -0,0 +1,57 @@
+import os
+import json
+import torch
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+##addition
+from config import cfg
+
+class trainByYossi(torch.utils.data.Dataset):
+
+    def __init__(self, odgt, opt, **kwargs):
+        # parse options
+        self.imgSizes = opt.imgSizes
+        self.imgMaxSize = opt.imgMaxSize
+        # max down sampling rate of network to avoid rounding during conv or pooling
+        self.padding_constant = opt.padding_constant
+
+        # parse the input list
+        self.parse_input_list(odgt, **kwargs)
+
+        # mean and std
+        self.normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
+
+    def parse_input_list(self, odgt, max_sample=-1, start_idx=-1, end_idx=-1):
+        if isinstance(odgt, list):
+            self.list_sample = odgt
+        elif isinstance(odgt, str):
+            self.list_sample = [json.loads(x.rstrip()) for x in open(odgt, 'r')]
+
+        if max_sample > 0:
+            self.list_sample = self.list_sample[0:max_sample]
+        if start_idx >= 0 and end_idx >= 0:  # divide file list
+            self.list_sample = self.list_sample[start_idx:end_idx]
+
+        self.num_sample = len(self.list_sample)
+        assert self.num_sample > 0
+        print('# samples: {}'.format(self.num_sample))
+
+    def img_transform(self, img):
+        # 0-255 to 0-1
+        img = np.float32(np.array(img)) / 255.
+        img = img.transpose((2, 0, 1))
+        img = self.normalize(torch.from_numpy(img.copy()))
+        return img
+
+    def segm_transform(self, segm):
+        # to tensor, -1 to 149
+        segm = torch.from_numpy(np.array(segm)).long() - 1
+        return segm
+
+    # Round x to the nearest multiple of p and x' >= x
+    def round2nearest_multiple(self, x, p):
+        return ((x - 1) // p + 1) * p
+
